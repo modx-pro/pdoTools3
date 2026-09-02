72ade796125bc8113ec2c8fcf14d0d1daac2ff99
Ivan Bochkarev <ivanx86@gmail.com>
2026-09-02 13:06:20 +0600

    chore(release): Bump package to 3.1.0-pl
    SemVer minor for new template flags, Fenom error labels, PHP 8.1+,
    and MiniShop3-only extra categories.

--- _build/build.config.php
+++ _build/build.config.php
@@ -3,7 +3,7 @@
 const PKG_NAME = 'pdoTools';
 const PKG_NAME_LOWER = 'pdotools';
 
-const PKG_VERSION = '3.0.4';
+const PKG_VERSION = '3.1.0';
 const PKG_RELEASE = 'pl';
 const PKG_AUTO_INSTALL = false;
 
